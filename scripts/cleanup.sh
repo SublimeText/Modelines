a7ab82613f868d05b0bf2e812e7375402822bff2
Frizlab <fload@me.com>
2026-01-22 23:18:26 +0100

    Migrate cleanup script to bash

---
+++ scripts/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+
+cd "$(dirname "$0")"
+# Note: Though not strictly equivalent, this could also be `git clean -xffd`…
+find . \( -name "*.pyc" -o -name "build" -o -name "dist" \) -exec rm -frv {} +
