c8c84c96fa4cfc396a114003476cd593062aebf0
Frizlab <fload@me.com>
2026-01-22 23:21:43 +0100

    Add __pycache__ in the cleanup files in cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 cd "$(dirname "$0")/.."
 # Note: Though not strictly equivalent, this could also be `git clean -xffd`…
-find . \( -name "*.pyc" -o -name "build" -o -name "dist" \) -exec rm -frv {} +
+find . \( -name "*.pyc" -o -name "__pycache__" -o -name "build" -o -name "dist" \) -exec rm -frv {} +
