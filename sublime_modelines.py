bfa884326efb991cb345d061aa1b694933ebb299
Frizlab <fload@me.com>
2026-01-23 03:16:50 +0100

    Remove vim support

--- sublime_modelines.py
+++ sublime_modelines.py
@@ -18,10 +18,10 @@ def debug_log(s, *args):
 debug_log("Modelines plugin start.")
 
 
-MODELINE_PREFIX_TPL = "%s\\s*(st|sublime|vim):"
+MODELINE_PREFIX_TPL = "%s\\s*(st|sublime):"
 
-MODELINE_TYPE_1  = re.compile(r"[\x20\t](st|sublime|vim):\x20?set\x20(.*):.*$")
-MODELINE_TYPE_2  = re.compile(r"[\x20\t](st|sublime|vim):(.*):.*$")
+MODELINE_TYPE_1  = re.compile(r"[\x20\t](st|sublime):\x20?set\x20(.*):.*$")
+MODELINE_TYPE_2  = re.compile(r"[\x20\t](st|sublime):(.*):.*$")
 
 KEY_VALUE = re.compile(r"""(?x) \s*
     (?P<key>\w+)  \s* (?P<op>\+?=)  \s*  (?P<value>
@@ -43,106 +43,6 @@ ST3 = sublime.version() >= "3000"
 
 if ST3:
     basestring = str
-
-VIM_MAP = {
-    #"gfn": "guifont"
-    #"guifont": {"regex": ..., 1: "font_face", 2: ("font_size", int)}
-
-    "ts": "tabstop",
-    "tabstop": ("tab_size", int),
-    "ai": "autoindent",
-    "autoindent": ("auto_indent", bool),
-    "et": "expandtab",
-    "expandtab": ("translate_tabs_to_spaces", bool),
-    "syn": "syntax",
-    "syntax": ("syntax", str),
-    "nu": "number",
-    "number": ("line_numbers", bool),
-
-    # "always_show_minimap_viewport": false,
-    # "animation_enabled": true,
-    # "atomic_save": true,
-    # "auto_close_tags": true,
-    # "auto_complete": true,
-    # "auto_complete_commit_on_tab": false,
-    # "auto_complete_delay": 50,
-    # "auto_complete_selector": "source - comment, meta.tag - punctuation.definition.tag.begin",
-    # "auto_complete_size_limit": 4194304,
-    # "auto_complete_triggers": [ {"selector": "text.html", "characters": "<"} ],
-    # "auto_complete_with_fields": false,
-    # "auto_find_in_selection": false,
-    # "auto_indent": true,
-    # "auto_match_enabled": true,
-    # "binary_file_patterns": ["*.jpg", "*.jpeg", "*.png", "*.gif", "*.ttf", "*.tga", "*.dds", "*.ico", "*.eot", "*.pdf", "*.swf", "*.jar", "*.zip"],
-    # "bold_folder_labels": false,
-    # "caret_style": "smooth",
-    # "color_scheme": "Packages/Color Scheme - Default/Monokai.tmTheme",
-    # "copy_with_empty_selection": true,
-    # "default_encoding": "UTF-8",
-    # "default_line_ending": "system",
-    # "detect_indentation": true,
-    # "dictionary": "Packages/Language - English/en_US.dic",
-    # "drag_text": true,
-    # "draw_centered": false,
-    # "draw_indent_guides": true,
-    # "draw_minimap_border": false,
-    # "draw_white_space": "selection",
-    # "enable_hexadecimal_encoding": true,
-    # "enable_telemetry": "auto",
-    # "ensure_newline_at_eof_on_save": false,
-    # "fade_fold_buttons": true,
-    # "fallback_encoding": "Western (Windows 1252)",
-    # "file_exclude_patterns": ["*.pyc", "*.pyo", "*.exe", "*.dll", "*.obj","*.o", "*.a", "*.lib", "*.so", "*.dylib", "*.ncb", "*.sdf", "*.suo", "*.pdb", "*.idb", ".DS_Store", "*.class", "*.psd", "*.db", "*.sublime-workspace"],
-    # "find_selected_text": true,
-    # "fold_buttons": true,
-    # "folder_exclude_patterns": [".svn", ".git", ".hg", "CVS"],
-    # "font_face": "",
-    # "font_options": [], # list
-    # "font_size": 10,
-    # "gpu_window_buffer": "auto",
-    # "gutter": true,
-    # "highlight_line": false,
-    # "highlight_modified_tabs": false,
-    # "ignored_packages": ["Vintage"]
-    # "indent_guide_options": ["draw_normal"],
-    # "indent_subsequent_lines": true,
-    # "indent_to_bracket": false,
-    # "index_files": true,
-    # "line_padding_bottom": 0,
-    # "line_padding_top": 0,
-    # "margin": 4,
-    # "match_brackets": true,
-    # "match_brackets_angle": false,
-    # "match_brackets_braces": true,
-    # "match_brackets_content": true,
-    # "match_brackets_square": true,
-    # "match_selection": true,
-    # "match_tags": true,
-    # "move_to_limit_on_up_down": false,
-    # "overlay_scroll_bars": "system",
-    # "preview_on_click": true,
-    # "rulers": [], # list
-    # "save_on_focus_lost": false,
-    # "scroll_past_end": true,
-    # "scroll_speed": 1.0,
-    # "shift_tab_unindent": false,
-    # "show_panel_on_build": true,
-    # "show_tab_close_buttons": true,
-    # "smart_indent": true,
-    # "spell_check": false,
-    # "tab_completion": true,
-    # "tab_size": 4,
-    # "theme": "Default.sublime-theme",
-    # "translate_tabs_to_spaces": false,
-    # "tree_animation_enabled": true,
-    # "trim_automatic_white_space": true,
-    # "trim_trailing_white_space_on_save": false,
-    # "use_simple_full_screen": false,
-    # "use_tab_stops": true,
-    # "word_separators": "./\\()\"'-:,.;<>~!@#$%^&*|+=[]{}`~?",
-    # "word_wrap": "auto",
-    # "wrap_width": 0,
-}
 
 def get_output_panel(name):
     if ST3: return sublime.active_window().create_output_panel(name)
@@ -172,14 +72,6 @@ def gen_modelines(view):
     for modeline in modelines:
         yield modeline
 
-def vim_mapped(t, s):
-    if t == "vim" or len(s) < 3:
-        while s in VIM_MAP:
-            s = VIM_MAP[s]
-        return s[0]
-    else:
-        return s
-
 
 def gen_raw_options(modelines):
     #import spdb ; spdb.start()
@@ -196,8 +88,7 @@ def gen_raw_options(modelines):
                 
                 m = KEY_VALUE.match(s)
                 if m:
-                    key, op, value = m.groups()
-                    yield vim_mapped(type, key), op, value
+                    yield m.groups()
                     s = s[m.end():]
                     continue
                 
@@ -206,14 +97,7 @@ def gen_raw_options(modelines):
                     k, = m.groups()
                     value = "true"
                     
-                    _k = vim_mapped(type, k)
-                    if (k.startswith("no") and (type == "vim" or (
-                        k[2:] in VIM_MAP or len(k) <= 4))):
-                        
-                        value = "false"
-                        _k = vim_mapped(type, k[2:])
-                    
-                    yield _k, "=", value
+                    yield k, "=", value
                     
                     s = s[m.end():]
                     continue
@@ -338,8 +222,6 @@ class ExecuteSublimeTextModeLinesCommand(sublime_plugin.EventListener):
         base_dir = settings.get("result_base_dir")
         
         for setter, name, value in gen_modeline_options(view):
-            #if "vim" in MODELINE_PREFIX_TPL: # vimsupport
-            #    vim_map.get(name)
             debug_log("modeline: %s = %s", name, value)
             
             if name == "x_syntax":
