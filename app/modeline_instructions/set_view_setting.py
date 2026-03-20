7ab6aea33ff13c777954209fe3e94d576d2ebcf2
Frizlab <fload@me.com>
2026-03-20 13:50:59 +0100

    Implement case-insensitive syntax searching

--- app/modeline_instructions/set_view_setting.py
+++ app/modeline_instructions/set_view_setting.py
@@ -1,4 +1,4 @@
-from typing import final
+from typing import final, Dict
 
 from os import path
 from sublime import View as SublimeView
@@ -22,24 +22,10 @@ class ModelineInstruction_SetViewSetting(ModelineInstruction):
 	def apply(self, view: SublimeView) -> None:
 		settings = view.settings()
 		
-		# Process setting value for special `syntax` case.
-		# Note <https://github.com/kvs/STEmacsModelines/blob/0a5487831c6ee5cedb924be4f1c64aa7651a3464/EmacsModelines.py#L40> might be a better algorithm.
-		# Among other things, it allows users to have a custom mapping of syntaxes, which we don’t.
-		if (self.setting_name == "syntax" and
-			 isinstance(self.setting_value, str) and
-			 not self.setting_value.endswith("tmLanguage") and
-			 not self.setting_value.endswith("sublime-syntax") and
-			 not "/" in self.setting_value and
-			 hasattr(sublime, "find_resources")
-			):
-			# We modify the value to find the proper file (this avoids specifying `Swift.tmLanguage`; instead we can use `Swift`).
-			# TODO: Case-insensitive search.
-			candidates = sublime.find_resources(f"{self.setting_value}.sublime-syntax") + sublime.find_resources(f"{self.setting_value}.tmLanguage")
-			if len(candidates) > 0:
-				# Note: We only use the basename of the found resource.
-				# For some (unknown) reason, using the full path and the basename does not yield the same results,
-				#  even when there is only one possible alternative!
-				self.setting_value = path.basename(path.normpath(candidates[0]))
+		if (self.setting_name == "syntax"):
+			# Process setting value for special `syntax` case:
+			#  we modify the value to find the proper file (this avoids specifying `Swift.tmLanguage`; instead we can use `Swift`, or even `swift`).
+			self.setting_value = self.syntax_for(self.setting_value)
 		
 		new_setting_value: SublimeValue
 		# The “match” instruction has been added to Python 3.10.
@@ -90,3 +76,44 @@ class ModelineInstruction_SetViewSetting(ModelineInstruction):
 	
 	def __str__(self) -> str:
 		return f"ModelineInstruction: SetViewSetting: {self.setting_name}{self.setting_modifier}={self.setting_value}"
+	
+	
+	# Initially I was using `find_resources` to find the exact file for a given syntax.
+	# The problem with this approach is the search is not case-insensitive (at least on a case-sensitive fs, not sure on a ci one).
+	# I then tried to generate a case-insensitive glob to use in `find_resources`.
+	# This probably should work, but the documentation of `find_resources` seems to be lying: it does not support actual glob.
+	# It supports the `*`, yes, but not `[]`, AFAICT.
+	# So we resort to finding *all* the known syntaxes and indexing them on their lowercased base name.
+	#
+	# For reference here’s the code with the conversion to glob:
+	# ```
+	# case_insensitive_glob_value_chars = [
+	#    c if not c.isalpha() else f"[{c.lower()}{c.upper()}]"
+	#    for c in list(glob_escape(self.setting_value))
+	# ]
+	# case_insensitive_glob_value = "".join(case_insensitive_glob_value_chars)
+	# candidates = sublime.find_resources(f"{case_insensitive_glob_value}.sublime-syntax") + sublime.find_resources(f"{case_insensitive_glob_value}.tmLanguage")
+	# ```
+	__known_syntaxes: Dict[str, str] = {}
+	@staticmethod
+	def syntax_for(setting_value: SublimeValue) -> SublimeValue:
+		if (isinstance(setting_value, str) and
+			 not setting_value.endswith("tmLanguage") and
+			 not setting_value.endswith("sublime-syntax") and
+			 not "/" in setting_value and
+			 hasattr(sublime, "find_resources")
+			):
+			
+			# If there is already a value for the given setting, we return it.
+			# _Technically_ we should make sure the value still exists, but let’s be optimistic.
+			if ret := ModelineInstruction_SetViewSetting.__known_syntaxes.get(setting_value.lower()):
+				return ret
+			
+			# If there are no values for the given settings, we refresh the full known syntaxes dictionary and try again.
+			Logger.debug("Re-computing full list of known syntaxes.")
+			ModelineInstruction_SetViewSetting.__known_syntaxes = {}
+			for s in sublime.find_resources("*.tmLanguage") + sublime.find_resources("*.sublime-syntax"):
+				base = path.basename(path.normpath(s))
+				ModelineInstruction_SetViewSetting.__known_syntaxes[path.splitext(base)[0].lower()] = base
+			
+			return ModelineInstruction_SetViewSetting.__known_syntaxes.get(setting_value.lower(), setting_value)
